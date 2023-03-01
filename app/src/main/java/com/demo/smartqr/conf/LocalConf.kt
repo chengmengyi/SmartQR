b7b8dc37bc2535d4c007b108cb6452801bb3a78e
chengmengyi <243237049@qq.com>
2023-03-01 15:03:47 +0800

    a

--- app/src/main/java/com/demo/smartqr/conf/LocalConf.kt
+++ app/src/main/java/com/demo/smartqr/conf/LocalConf.kt
@@ -1,6 +1,6 @@
 package com.demo.smartqr.conf
 
 object LocalConf {
-    const val email=""
-    const val url=""
+    const val email="josejoeyo328@gmail.com"
+    const val url="https://sites.google.com/view/smartqrapp/home"
 }
